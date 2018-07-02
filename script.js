cb30f45acbccdad690296d1d9e58708efa48e40b
Jacob Carlton <32211368+jacobc1204@users.noreply.github.com>
2018-07-02 18:07:46 -0500

    nothing major

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btns = document.querySelectorAll('.nav-btn');
 
 btns.forEach(btn => btn.addEventListener('click', () => {
-    let target = btn.id;
-    window.location += target;
+  const target = btn.id;
+  window.location += target;
 }));
